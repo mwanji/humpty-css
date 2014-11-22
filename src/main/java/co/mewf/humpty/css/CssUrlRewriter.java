95a9cb780efab65f87803c00ce568b169eb90698
moandji.ezana <mwanji@gmail.com>
2014-11-22 22:26:00 +0200

    background and background-image urls are rewritten

--- src/main/java/co/mewf/humpty/css/CssUrlRewriter.java
+++ src/main/java/co/mewf/humpty/css/CssUrlRewriter.java
@@ -1,13 +1,14 @@
 package co.mewf.humpty.css;
 
 import java.net.URI;
+import java.util.List;
 
 import org.apache.commons.io.FilenameUtils;
 import org.webjars.WebJarAssetLocator;
 
 import com.helger.css.ECSSVersion;
+import com.helger.css.decl.CSSDeclaration;
 import com.helger.css.decl.CSSExpressionMemberTermURI;
-import com.helger.css.decl.CSSFontFaceRule;
 import com.helger.css.decl.CascadingStyleSheet;
 import com.helger.css.reader.CSSReader;
 import com.helger.css.writer.CSSWriter;
@@ -23,40 +24,55 @@ public class CssUrlRewriter {
 
   public String rewrite(String css) {
     CascadingStyleSheet styleSheet = CSSReader.readFromString(css, ECSSVersion.CSS30);
-    CascadingStyleSheet rewrittenStyleSheet = new CascadingStyleSheet();
     CSSWriterSettings settings = new CSSWriterSettings(ECSSVersion.CSS30);
     settings.setQuoteURLs(true);
     CSSWriter writer = new CSSWriter(settings);
     writer.setWriteHeaderText(false);
     writer.setWriteFooterText(false);
     
-    styleSheet.getAllRules().stream().forEach(rule -> {
-      if (rule instanceof CSSFontFaceRule) {
-        CSSFontFaceRule rewrittenRule = new CSSFontFaceRule();
-        ((CSSFontFaceRule) rule).getAllDeclarations().stream().forEach(d -> {
-          if (d.getProperty().equals("font-family")) {
-            rewrittenRule.addDeclaration(d);
-          } else if (d.getProperty().equals("src")) {
-            d.getExpression().getAllMembers().stream().filter(m -> m instanceof CSSExpressionMemberTermURI).map(m -> (CSSExpressionMemberTermURI) m).forEach(m -> {
-              String originalName = m.getURIString();
-              String name = FilenameUtils.getName(originalName);
-              if (name.indexOf('?') > -1) {
-                name = name.substring(0, name.indexOf('?'));
-              }
-              if (name.indexOf('#') > -1) {
-                name = name.substring(0, name.indexOf('#'));
-              }
-              String fullPath = locator.getFullPath(name).replaceFirst("META-INF/resources", "");
-              
-              m.setURIString(URI.create(fullPath.substring(0, fullPath.lastIndexOf('/') + 1) + originalName).normalize().toString());
-            });
-          }
-        });
-      }
-      rewrittenStyleSheet.addRule(rule);
-    });
+    styleSheet.getAllFontFaceRules()
+              .stream()
+              .map(rule -> rule.getAllDeclarationsOfPropertyNameCaseInsensitive("src"))
+              .flatMap(List::stream)
+              .filter(declaration -> declaration.getProperty().equals("src"))
+              .map(declaration -> declaration.getExpression().getAllMembers())
+              .flatMap(List::stream)
+              .filter(member -> member instanceof CSSExpressionMemberTermURI)
+              .map(member -> (CSSExpressionMemberTermURI) member)
+              .forEach(member -> {
+                String originalName = member.getURIString();
+                String name = FilenameUtils.getName(originalName);
+                if (name.indexOf('?') > -1) {
+                  name = name.substring(0, name.indexOf('?'));
+                }
+                if (name.indexOf('#') > -1) {
+                  name = name.substring(0, name.indexOf('#'));
+                }
+                String fullPath = locator.getFullPath(name).replaceFirst("META-INF/resources", "");
+
+                member.setURIString(URI.create(fullPath.substring(0, fullPath.lastIndexOf('/') + 1) + originalName).normalize().toString());
+              });
     
-    return writer.getCSSAsString(rewrittenStyleSheet);
+    styleSheet.getAllStyleRules()
+              .stream()
+              .map(rule -> {
+                List<CSSDeclaration> declarations = rule.getAllDeclarationsOfPropertyNameCaseInsensitive("background");
+                declarations.addAll(rule.getAllDeclarationsOfPropertyNameCaseInsensitive("background-image"));
+                return declarations;
+              })
+              .flatMap(List::stream)
+              .map(declaration -> declaration.getExpression().getAllMembers())
+              .flatMap(List::stream)
+              .filter(member -> member instanceof CSSExpressionMemberTermURI)
+              .map(member -> (CSSExpressionMemberTermURI) member)
+              .forEach(member -> {
+                String originalName = member.getURIString();
+                String name = FilenameUtils.getName(originalName);
+                String fullPath = locator.getFullPath(name).replaceFirst("META-INF/resources", "");
+                member.setURIString(URI.create(fullPath.substring(0, fullPath.lastIndexOf('/') + 1) + originalName).normalize().toString());
+              });
+    
+    return writer.getCSSAsString(styleSheet);
   }
 
 }
--- src/test/java/co/mewf/humpty/css/CssUrlRewriterTest.java
+++ src/test/java/co/mewf/humpty/css/CssUrlRewriterTest.java
@@ -8,12 +8,26 @@ import org.webjars.WebJarAssetLocator;
 
 public class CssUrlRewriterTest {
 
+  private final CssUrlRewriter rewriter = new CssUrlRewriter(new WebJarAssetLocator());
+  
   @Test
-  public void should_shorten_font_urls_to_webjar_name() throws Exception {
-    CssUrlRewriter rewriter = new CssUrlRewriter(new WebJarAssetLocator());
-    
+  public void should_change_font_urls_to_servlet3_resource_path() throws Exception {
     String rewrittenCss = rewriter.rewrite(IOUtils.toString(getClass().getResourceAsStream("fonts.css")));
     
     assertEquals(IOUtils.toString(getClass().getResourceAsStream("fonts_rewritten.css")), rewrittenCss);
+  }
+  
+  @Test
+  public void should_change_background_url_to_servlet3_resource_path() throws Exception {
+    String rewrittenCss = rewriter.rewrite(IOUtils.toString(getClass().getResourceAsStream("background.css")));
+    
+    assertEquals(IOUtils.toString(getClass().getResourceAsStream("background_rewritten.css")), rewrittenCss);
+  }
+  
+  @Test
+  public void should_change_background_image_url_to_servlet3_resource_path() throws Exception {
+    String rewrittenCss = rewriter.rewrite(IOUtils.toString(getClass().getResourceAsStream("background-image.css")));
+    
+    assertEquals(IOUtils.toString(getClass().getResourceAsStream("background-image_rewritten.css")), rewrittenCss);
   }
 }
